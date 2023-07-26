1bc7646b8cc237d5aa7db98899bf446fc2bb20d0
bapugouda-b <rajjdanush@gmail.com>
2023-07-26 18:49:02 +0530

    Completed Material-UI TextField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
-import ButtonMui from './components/ButtonMui';
-import MuiTypography from './components/MuiTypography';
+// import ButtonMui from './components/ButtonMui';
+// import MuiTypography from './components/MuiTypography';
+import TextFieldMui from './components/TextFieldMui';
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
-      <ButtonMui />
+      {/* <MuiTypography /> */}
+      {/* <ButtonMui /> */}
+      <TextFieldMui />
     </div>
   );
 }
