6a6aa92e6e7614bca5d9095d2d3c12219f53216f
Bapugouda <rajjdanush@gmail.com>
2023-08-06 23:34:23 +0530

    Updated DatePicker

--- src/components/DatePickerMui.tsx
+++ src/components/DatePickerMui.tsx
@@ -1,41 +1,46 @@
-import { Stack, TextField, TextFieldProps } from "@mui/material";
-import { DatePicker, TimePicker, DateTimePicker } from "@mui/x-date-pickers";
+import React from "react";
+import { Box, Typography, TextField } from "@mui/material";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { useState } from "react";
 
 const DatePickerMui = () => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [selectedTime, setSelectedTime] = useState<Date | null>(null);
-  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null);
-
-  console.log({ selectedTime: selectedTime && selectedTime.toLocaleDateString() });
+  const [value, setValue] = useState<Date | null>(null);
 
   return (
-    <Stack spacing={4} sx={{ width: "250px" }} my={2}>
-      <DatePicker
-        label="Date picker"
-        renderInput={(params: TextFieldProps) => <TextField {...params} />}
-        value={selectedDate}
-        onChange={(newValue: Date | null) => {
-          setSelectedDate(newValue);
-        }}
-      />
-      <TimePicker
-        label="Time picker"
-        renderInput={(params: TextFieldProps) => <TextField {...params} />}
-        value={selectedTime}
-        onChange={(newValue: Date | null) => {
-          setSelectedTime(newValue);
-        }}
-      />
-       <DateTimePicker
-        label="Date Time picker"
-        renderInput={(params: TextFieldProps) => <TextField {...params} />}
-        value={selectedDateTime}
-        onChange={(newValue: Date | null) => {
-          setSelectedDateTime(newValue);
-        }}
-      />
-    </Stack>
+    <Box>
+      <Typography variant="h5" color="secondary" align="center" my={2}>
+        MUI Date Picker Example
+      </Typography>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Select Date"
+          minDate={dayjs("2022-08-22")}
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+          renderInput={(props: any) => <TextField {...props} />}
+        />
+
+        <MobileDatePicker
+          label="Mobile Date Picker"
+          value={value}
+          onChange={(newValue: any) => setValue(newValue)}
+          renderInput={(props: any) => <TextField {...props} />}
+        />
+        <DesktopDatePicker
+          label="Desktop Date Picker"
+          openTo="year"
+          views={["month", "year"]}
+          value={value}
+          onChange={(newValue: any) => setValue(newValue)}
+          renderInput={(props: any) => <TextField {...props} />}
+        />
+      </LocalizationProvider>
+    </Box>
   );
 };
 
